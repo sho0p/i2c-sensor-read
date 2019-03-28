f99aa2488bb1d814f6c40ca71a2d378256eb894b
Zachary Shoop <shoopshopp@gmail.com>
2019-03-28 15:51:16 -0400

    h file

--- i2c.cpp
+++ i2c.cpp
@@ -1,4 +1,13 @@
+#define EN_REG          (0x0C)
+#define STAT_REG        (0x00)
+#define OUT_REG         (0x01)
+
+
+
+
+
 #define I2C_DEV_LOC     (0x2a)
+
 
 #include <iostream>
 #include <errno.h>
@@ -14,5 +23,11 @@ int main(){
     
     fd = wiringPiI2CSetup(I2C_DEV_LOC);
     cout << "Initial result: " << fd << endl;
+
+
     return 0;
+}
+
+void setup(){
+
 }
