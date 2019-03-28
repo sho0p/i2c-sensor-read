90f5ba29e9e57e0d6987a168c8045618d53ab724
sho0p <shoopshopp@gmail.com>
2019-03-28 20:36:50 +0000

    fixed ordering

--- i2c.cpp
+++ i2c.cpp
@@ -8,6 +8,15 @@ using namespace std;
 
 int fd;
 
+uint16_t cdcread(uint8_t chread){
+    uint8_t lsb = 
+        wiringPiI2CReadReg8(fd, 
+        DATAX_LSB + (chread<<1));
+    uint8_t msb = 
+        wiringPiI2CReadReg16(fd,
+        DATAX_MSB + (chread<<1));
+    return (msb << 8) | (lsb);
+}
 
 int main(){
     int result;
@@ -15,7 +24,7 @@ int main(){
     fd = wiringPiI2CSetup(I2C_DEV_LOC);
     cout << "Initial result: " << fd << endl;
     while(1){
-        cdcread(0);
+        cdcread(0x0);
     }
     result = 0;
     return result;
@@ -26,12 +35,4 @@ void setup(){
                     CH1EN);
 }
 
-uint16_t cdcread(uint8_t chread){
-    uint8_t lsb = 
-        wiringPiI2CReadReg8(fd, 
-        DATAX_LSB + (chread<<1));
-    uint8_t msb = 
-        wiringPiI2CReadReg16(fd,
-        DATAX_MSB + (chread<<1));
-    return (msb << 8) | (lsb);
-}
+
