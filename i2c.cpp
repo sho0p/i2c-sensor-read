163799430e8de5ff8b295bb6be0253cebdf4a5e7
sho0p <shoopshopp@gmail.com>
2019-03-28 22:04:00 +0000

    bunch of changes on pi because I got sick of gitting it over

--- i2c.cpp
+++ i2c.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <errno.h>
 #include <stdint.h>
-#include <wiringPi.h>
 #include <wiringPiI2C.h>
+#include <wiringPi.h>
 #include "i2cdef.h"
 
 using namespace std;
 
 int fd;
 
-void cdcread(uint8_t chread){
+void cdcread(){
     uint8_t lsb =
         wiringPiI2CReadReg8(fd, 
         DATA0_LSB );
     uint8_t msb = 
         wiringPiI2CReadReg8(fd,
         DATA0_MSB );
-    cout << "Read on channel " << chread << ": " << ((msb << 8) | (lsb));
+    cout << "Read on channel 0: " << ((msb << 8) | (lsb));
 }
 void setup(){
     cout <<"Waiting to write to enable..." << endl;
-    while(!WiringPiI2CReadReg8(fd, STAT_REG & 0x20)){
+    while(!wiringPiI2CReadReg8(fd, STAT_REG & 0x20)){
     }
     wiringPiI2CWriteReg8(fd, EN_REG, 
                     CH1EN);
@@ -29,7 +29,7 @@ int main(){
     int result;
     
     fd = wiringPiI2CSetup(I2C_DEV_LOC);
-        wiringPiISR(INTB_PIN, INT_EDGE_RISING, *cdcread(0x0));
+        wiringPiISR(INTB_PIN, INT_EDGE_RISING, *cdcread);
     cout << "Initial result: " << fd << endl;
     setup();
     while(1);
